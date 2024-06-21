16db6cfd897ce64ee85c7abfb725d4697de52c50
SamSun0530 <yj020530@student.cs.ubc.ca>
2024-06-20 21:33:24 -0700

    add to cart and delete from cart function, however the limit depends on initial state in slice

--- DealZone/src/slices/cart.js
+++ DealZone/src/slices/cart.js
@@ -2,11 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     items: [
-        { id: 1, name: 'Item 1', price: 10, image: 'https://via.placeholder.com/150' },
+        /*{ id: 1, name: 'Item 1', price: 10, image: 'https://via.placeholder.com/150' },
         { id: 2, name: 'Item 2', price: 20, image: 'https://via.placeholder.com/150' },
         { id: 3, name: 'Item 3', price: 30, image: 'https://via.placeholder.com/150' },
         { id: 4, name: 'Item 4', price: 40, image: 'https://via.placeholder.com/150' },
-        { id: 5, name: 'Item 5', price: 50, image: 'https://via.placeholder.com/150' },
+        { id: 5, name: 'Item 5', price: 50, image: 'https://via.placeholder.com/150' },*/
     ],
 };
 
