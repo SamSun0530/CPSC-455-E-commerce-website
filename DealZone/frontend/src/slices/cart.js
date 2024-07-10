20321592019176c6bfecc5294d9f123ae87cbe3f
SamSun0530 <yj020530@student.cs.ubc.ca>
2024-07-09 21:23:18 -0700

    cart with backend

--- DealZone/frontend/src/slices/cart.js
+++ DealZone/frontend/src/slices/cart.js
@@ -2,13 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { getCartAsync, addToCartAsync, deleteFromCartAsync, clearCartAsync } from '../thunks/cartThunk';
 
 const initialState = {
-    items: [
-        { id: 1, name: 'This is a test of long title product should not cause the alignment problem, image, title and buttons should not interfere each other', price: 10, image: 'https://via.placeholder.com/150' },
-        { id: 2, name: 'Item 2', price: 20, image: 'https://via.placeholder.com/150' },
-        { id: 3, name: 'Item 3', price: 30, image: 'https://via.placeholder.com/150' },
-        { id: 4, name: 'Item 4', price: 40, image: 'https://via.placeholder.com/150' },
-        { id: 5, name: 'Item 5', price: 50, image: 'https://via.placeholder.com/150' }
-    ],
+    items: [],
     status: 'idle',
 };
 
