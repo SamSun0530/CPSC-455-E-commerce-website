0e12f08876f6fb4aa6e42cb8ee444ad0f25eedff
Andy Lee <andy2002221@gmail.com>
2024-07-22 18:49:46 -0700

    cors access error

--- DealZone/backend/app.js
+++ DealZone/backend/app.js
@@ -14,20 +14,17 @@ var wishlistRouter = require('./routes/wishlist');
 var postsRouter = require('./routes/posts');
 
 var app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 
 app.use('/', indexRouter);
 app.use('/session', sessionRouter);
