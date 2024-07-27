c5b8af59005d048f09c897bb8bce5b356709f31b
Andy Lee <andy2002221@gmail.com>
2024-07-27 10:50:07 -0700

    log cookies

--- DealZone/backend/middleware/session.js
+++ DealZone/backend/middleware/session.js
@@ -2,6 +2,7 @@ const { authSession } = require("../service/session");
 const { getUserBasic } = require("../service/user");
 
 const verifySession = async (req, res, next) => {
+    console.log(req.cookies);
     if (req.cookies && req.cookies.sessionToken) {
         const sessionToken = req.cookies.sessionToken;
         const auth = await authSession(sessionToken);
