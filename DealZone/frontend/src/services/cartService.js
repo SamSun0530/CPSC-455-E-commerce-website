f065c8d0dc3c6dd9ded0d9f552044b24a95b86a4
SamSun0530 <yj020530@student.cs.ubc.ca>
2024-07-27 16:11:31 -0700

    revert unwanted change

--- DealZone/frontend/src/services/cartService.js
+++ DealZone/frontend/src/services/cartService.js
@@ -1,16 +1,19 @@
 const getCart = async () => {
-    const response = await fetch('http://localhost:3000/cart', {
+    const response = await fetch('https://project-10-tech-titans.onrender.com/cart', {
         method: 'GET',
-        credentials: 'include'
+        headers: {
+            'session-token': sessionStorage.getItem('sessionToken')
+        },
     });
     return response.json();
 };
 
 const addToCart = async (item) => {
-    const response = await fetch('http://localhost:3000/cart', {
+    const response = await fetch('https://project-10-tech-titans.onrender.com/cart', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'session-token': sessionStorage.getItem('sessionToken')
         },
         body: JSON.stringify(item),
         credentials: 'include'
@@ -24,21 +27,22 @@ const addToCart = async (item) => {
 };
 
 const deleteFromCart = async (id) => {
-    const response = await fetch(`http://localhost:3000/cart/${id}`, {
+    const response = await fetch(`https://project-10-tech-titans.onrender.com/cart/${id}`, {
         method: 'DELETE',
+        headers: {
+            'session-token': sessionStorage.getItem('sessionToken')
+        },
         credentials: 'include'
     });
-    const data = await response.json();
-    if (!response.ok) {
-        const errorMsg = data?.message;
-        throw new Error(errorMsg)
-    }
-    return data;
+    return response.json();
 }
 
 const clearCart = async () => {
-    const response = await fetch('http://localhost:3000/cart', {
+    const response = await fetch('https://project-10-tech-titans.onrender.com/cart', {
         method: 'DELETE',
+        headers: {
+            'session-token': sessionStorage.getItem('sessionToken')
+        },
         credentials: 'include'
     });
     return response.json();
