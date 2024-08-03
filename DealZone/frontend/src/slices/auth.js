38eae7e5ec222272c3b4057a157f92689b8e6224
Andy Lee <andy2002221@gmail.com>
2024-08-03 02:04:24 -0700

    loading screen while pending session API to avoid flashing logged out state when logged in

--- DealZone/frontend/src/slices/auth.js
+++ DealZone/frontend/src/slices/auth.js
@@ -10,6 +10,7 @@ export const authSlice = createSlice({
         registerUser: REQUEST_STATE.IDLE,
         checkSession: REQUEST_STATE.IDLE,
         logOutUser: REQUEST_STATE.IDLE,
+        loading: true,
         error: null
     },
     reducers: {
@@ -53,15 +54,19 @@ export const authSlice = createSlice({
             .addCase(checkSessionAsync.pending, (state) => {
                 state.checkSession = REQUEST_STATE.PENDING;
                 state.error = null;
+                state.loading = true;
             })
             .addCase(checkSessionAsync.fulfilled, (state) => {
                 state.checkSession = REQUEST_STATE.FULFILLED;
                 state.isLoggedIn = true;
+                state.loading = false;
             })
             .addCase(checkSessionAsync.rejected, (state, action) => {
                 state.checkSession = REQUEST_STATE.REJECTED;
                 state.error = action.error;
-            }).addCase(logOutUserAsync.pending, (state) => {
+                state.loading = false;
+            })
+            .addCase(logOutUserAsync.pending, (state) => {
                 state.logOutUser = REQUEST_STATE.PENDING;
                 state.error = null;
                 state.isLoggedIn = false;
