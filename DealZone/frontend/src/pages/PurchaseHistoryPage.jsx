264054f67d5f6a2b7131c1b90c527332905b5f13
ivenadu <ivena2012@gmail.com>
2024-08-05 15:20:47 -0700

    fix

--- DealZone/frontend/src/pages/PurchaseHistoryPage.jsx
+++ DealZone/frontend/src/pages/PurchaseHistoryPage.jsx
@@ -7,7 +7,7 @@ import '../css/PurchaseHistoryPage.css';
 
 const PurchaseHistoryPage = () => {
     const dispatch = useDispatch();
-    const purchaseHistoryItem = useSelector(state => state.purchaseHistory.items);
+    const purchaseHistoryItem = useSelector((state) => state.purchaseHistory.items);
 
     useEffect(() => {
         dispatch(getPurchaseHistoryAsync());
@@ -19,9 +19,19 @@ const PurchaseHistoryPage = () => {
             <div className="purchase-history-page">
                 <h1>Purchase History</h1>
                 <div className="purchase-history-list">
-                    {purchaseHistoryItem.map(item => (
-                        <PurchasedItem key={item._id} purchasedItem={item} />
-                    ))}
+                    {purchaseHistoryItem.map(item => {
+                        // console.log(item); 
+                        return(
+                        <div className="cart-item">
+                            <img src={item.image} alt={item.title} className="cart-item-image" />
+                            <div className="cart-item-details">
+                                <h3>{item.title}</h3>
+                                <p>${item.price}</p>
+                                <p>Purchased on: {new Date(item.purchased_on).toLocaleDateString()}</p>
+                            </div>
+                        </div>)
+                    })}
+
                 </div>
             </div>
         </div>
